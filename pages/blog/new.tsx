9b4671174c0c9635fddf17b3790cb4dbe6cfc87d
KUMachine <aryan.lokar@gmail.com>
2021-07-09 18:52:10 +0300

    bug fix again

--- pages/blog/new.tsx
+++ pages/blog/new.tsx
@@ -13,7 +13,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
                 'common',
                 'blog',
             ])),
-            user,
+            // user,
         },
     }
 }
